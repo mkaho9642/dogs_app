17b8180ef1429335646c0a9d7d0c8361aa83ac3d
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-05-17 10:13:52 +0000

    [Update] routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'home/about', to: 'homes#about', as: :about
   resources :reviews, only: [:new, :create, :show]
-  resources :users, only: [:mypage, :show, :index, :edit, :update, :confirm, :quit]
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      get 'mypage'
+      get 'bookmark', action: :index
+    end
+  end
+  get 'users/confirm'
+  patch 'users/quit'
 end
