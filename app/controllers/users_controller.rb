c8cf6bf1cab9392f450b2081991f8444be325e84
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-05-17 10:43:00 +0000

    [Update] users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,32 @@
 class UsersController < ApplicationController
   def mypage
-    @user = User.find(params[:id])
-    @post_review = @user.post_review
+    @user = current_user
+    render 'mypage'
   end
 
   def show
     @user = User.find(params[:id])
-    @post_review = @user.post_review
+    render 'user'
+  end
+
+  def index
+    @user = User.find(params[:id])
+    @bookmarks = @user.bookmarks
+    render 'bookmark'
   end
 
   def edit
     @user = User.find(params[:id])
+    render 'edit'
   end
 
   def confirm
+    @user = current_user
+    render 'confirm'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
