bd4642fd0d403f17406daf9f1c8add43c64b0587
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-06-03 14:32:28 +0000

    [Add] admin/salons

--- app/controllers/admin/salons_controller.rb
+++ app/controllers/admin/salons_controller.rb
@@ -1,2 +1,19 @@
 class Admin::SalonsController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
