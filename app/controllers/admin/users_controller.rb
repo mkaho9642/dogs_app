5746476e5f258b016957473b77d05dcbbc48d08b
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-06-14 16:01:12 +0000

    [Update] admin/users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,9 +15,12 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
     redirect_to admin_users_path(user)
+    else
+      render :edit
+    end
   end
 
   private
