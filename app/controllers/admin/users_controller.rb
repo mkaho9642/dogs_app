d45bae28a5779c5153035d0a8018959b15cfe75a
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-06-14 15:19:10 +0000

    [Update] admin/users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,28 @@
 class Admin::UsersController < ApplicationController
   layout 'admin'
   before_action :authenticate_admin!
+  
   def index
       @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to admin_users_path(user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :nickname, :is_active)
   end
 end
