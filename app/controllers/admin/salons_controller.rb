af71d1998be7372e00b05a9aee31b11d094253a6
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-06-15 15:16:09 +0000

    [Update] admin/salons

--- app/controllers/admin/salons_controller.rb
+++ app/controllers/admin/salons_controller.rb
@@ -1,35 +1,47 @@
 class Admin::SalonsController < ApplicationController
   layout 'admin'
-  def create
-    @salon = Salon.new(salon_params)
-    if @salon.save
-      flash[:notice] = "You have created book successfully."
-      redirect_to admin_salon_path(@salon), notice: "サロンを登録しました"
-    else
-      @salons = Salon.all
-      render :index
-    end
-  end
 
   def index
     @salons = Salon.all
+  end
+
+  def new
     @salon = Salon.new
   end
 
+  def create
+    @salon = Salon.new(salon_params)
+    if @salon.save
+      redirect_to admin_salon_path(@salon), notice: "サロンを登録しました"
+    else
+      render :new
+    end
+  end
+
   def show
+    @item = Salon.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @salon.update(salon_params)
+      redirect_to admin_salon_path(@salon)
+    else
+      render :edit
   end
 
   def destroy
   end
 
   private
-  def salon_params
-    params.require(:salon).permit(:name, :introduction,:image)
+    def set_salon
+      @salon = Salon.find(params[:id])
+    end
+
+    def salon_params
+      params.require(:salon).permit(:name, :introduction,:image)
+    end
   end
 end
--- app/models/salon.rb
+++ app/models/salon.rb
@@ -1,2 +1,10 @@
 class Salon < ApplicationRecord
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :introduction
+    validates :image
+  end
+  
 end
